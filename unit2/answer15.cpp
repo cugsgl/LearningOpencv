c5c8f4d2737164cd0ad743f46cbfb329741868d6
cugsgl <1105435871@qq.com>
2019-12-15 22:14:39 +0800

    12.15huashui

---
+++ unit2/answer15.cpp
@@ -0,0 +1,82 @@
+//max-min滤波
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+cv::Mat BGR2GRAY(cv::Mat img)
+{
+    // get height and width
+    int width = img.cols;
+    int height = img.rows;
+
+    // prepare output
+    cv::Mat out = cv::Mat::zeros(height, width, CV_8UC1);
+
+    // each y, x
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            // BGR -> Gray
+            out.at<uchar>(y, x) = 0.2126 * (float)img.at<cv::Vec3b>(y, x)[2] + 0.7152 * (float)img.at<cv::Vec3b>(y, x)[1] + 0.0722 * (float)img.at<cv::Vec3b>(y, x)[0];
+        }
+    }
+    return out;
+}
+
+cv::Mat sobelfilter(cv::Mat img, int n)
+{
+    //建立高斯核模板
+    //float *guass= new float[n,n];
+
+    int height = img.rows;
+    int width = img.cols;
+    int channel = img.channels();
+    int c = 0, c1 = 0, r = 0, r1 = 0;
+    cv::Mat out = cv::Mat::zeros(height, width, CV_8UC1);
+
+    double kernel[n][n] = {{1, 2, 1}, {0, 0, 0}, {-1, -2, -1}};
+
+    int pad = floor(n / 2);
+
+    double v = 0;
+
+    // filtering
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            v = 0;
+            for (int dy = -pad; dy < pad + 1; dy++)
+            {
+                for (int dx = -pad; dx < pad + 1; dx++)
+                {
+                    if (((y + dy) >= 0) && ((x + dx) >= 0) && ((y + dy) < height) && ((x + dx) < width))
+                    {
+                        v += img.at<uchar>(y + dy, x + dx) * kernel[dy + pad][dx + pad];
+                    }
+                }
+            }
+            v = fmax(v, 0);
+            v = fmin(v, 255);
+            out.at<uchar>(y, x) = (uchar)v;
+        }
+    }
+    return out;
+}
+
+int main(int agrc, const char *agrv[])
+{
+    cv::Mat img = cv::imread("F:\\learnOpencv\\unit2\\imori.jpg");
+    cv::Mat guass = sobelfilter(BGR2GRAY(img), 3);
+
+    cv::namedWindow("img");
+    cv::namedWindow("guass");
+
+    cv::imshow("img", img);
+    cv::imshow("guass", guass);
+
+    cv::imwrite("F:\\learnOpencv\\unit2\\answer_image\\answer15.jpg", guass);
+    cv::waitKey(0);
+    cv::destroyAllWindows();
+}
--- unit2/log.md
+++ unit2/log.md
@@ -6,3 +6,15 @@ Type "show configuration" for configuration details.` 显然它讲的是GDB是64
 # ansewr12 and answer13 
 
 1. 为指针动态申请内存 `int *a=new int[n]`
+
+# answer 15
+
+1. 进行sobel处理之后，有的像元值为负数，有的像元值为正数，使用下列代码，把值转化到0-255
+```c++
+ fmax(piexl,0);
+ fmin(poexl,255)
+ ```
+
+ 2. 滑动窗口可以使用更巧妙的方法实现，嵌套四个for循环加一个if判断是否出界。
+ 
+
